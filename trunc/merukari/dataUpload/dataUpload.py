eb44a8193d1185c67de885fe3b7c8b98fa769de2
katomanz <kittokatto8083@gmail.com>
2019-01-18 20:11:57 -0800

    Remove authrization process!

--- trunc/merukari/dataUpload/dataUpload.py
+++ trunc/merukari/dataUpload/dataUpload.py
@@ -25,8 +25,19 @@ query = args[1]
 filename = query
 
 gauth = GoogleAuth()
-#gauth.CommandLineAuth()
-gauth.LocalWebserverAuth()
+# Try to load saved client credentials
+gauth.LoadCredentialsFile("mycreds.txt")
+if gauth.credentials is None:
+    # Authenticate if they're not there
+    gauth.LocalWebserverAuth()
+elif gauth.access_token_expired:
+    # Refresh them if expired
+    gauth.Refresh()
+else:
+    # Initialize the saved creds
+    gauth.Authorize()
+# Save the current credentials to a file
+gauth.SaveCredentialsFile("mycreds.txt")
 
 drive = GoogleDrive(gauth)
 
