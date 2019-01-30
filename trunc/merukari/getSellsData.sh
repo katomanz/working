466902af44d54edb29a1662f485c574e4853d3dd
katomanz <kittokatto8083@gmail.com>
2019-01-30 00:04:33 -0800

    Added time to start

--- trunc/merukari/getSellsData.sh
+++ trunc/merukari/getSellsData.sh
@@ -7,9 +7,10 @@ if [ $# -lt 1 ]; then
 fi
 
 echo "Let's start searching!"
+date '+%T'
 # Run Searching and Create result csv file
 # Return value is csv file name of result
-CSV_FILENAME=$(python3.6 ./sclpMerukari/merukari.py $1 $2 2>&1 > /dev/null);
+CSV_FILENAME=$(python3.6 ./sclpMerukari/merukari.py $1 $2 2>&1 > ./merukari.log);
 echo $CSV_FILENAME
 
 # Upload csv file to google drive
@@ -18,9 +19,10 @@ python3.6 ./dataUpload.py $CSV_FILENAME
 cd ..
 
 echo "Let's start analysing!"
+date '+%T'
 # Run Analysis and Create result csv file
 # Return value is HTML file name of result
-HTML_FILENAME=$(python3.6 ./analize/analize.py $CSV_FILENAME 2>&1 > /dev/null);
+HTML_FILENAME=$(python3.6 ./analize/analize.py $CSV_FILENAME 2>&1 > ./analize.log);
 echo $HTML_FILENAME
 
 # Upload html file to google drive
